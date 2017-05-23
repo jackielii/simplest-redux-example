9c7831a334422dcfac42700b58d0a110aa4a9307
luixlacrux <luixlacrux18@gmail.com>
2017-05-23 16:35:21 -0400

    Use of prop-types library

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider, connect } from 'react-redux'
