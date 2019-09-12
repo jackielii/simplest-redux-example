f134148c76b7c5ffd4e6b76856fed12a8e2e2b6e
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-09-12 10:20:09 +0530

    Added menu header bar and apply styling css
    Apply styling and css to header component.
    set dynamic router change and link selection based on preferred link selection.

--- src/component/header.js
+++ src/component/header.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 
 class Header extends Component {
     render() {
         return (
             <React.Fragment>
                 <header>
-                    <Link to="/">Home</Link>
-                    <Link to="/user">User</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/contact">Contact</Link>
+                    <ul>
+                        <li className={this.props.location.pathname === "/" ? "active" : ""}><Link to="/">Home</Link></li>
+                        <li className={this.props.location.pathname === "/user" ? "active" : ""}><Link to="/user">User</Link></li>   
+                        <li className={this.props.location.pathname === "/about" ? "active" : ""}><Link to="/about">About</Link></li>
+                        <li className={this.props.location.pathname === "/contact" ? "active" : ""}><Link to="/contact">Contact</Link></li>
+                    </ul>
                 </header>
             </React.Fragment>
         )
     }
 }
 
-export default Header;
+export default withRouter(Header);
